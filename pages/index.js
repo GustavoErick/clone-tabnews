9b5302ee9b768533ccdee954cd9382a42f8aa248
GustavoErick <105515712+GustavoErick@users.noreply.github.com>
2025-12-09 13:30:40 +0000

    chore: estilizacao do  `prettier`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home() {
-    return (
-        <h1>
-            A felicidade, seja como for, <br /> 
-            é a capacidade de sentir <br />
-            gratidão pelo agora.
-        </h1>
-    )
+  return (
+    <h1>
+      A felicidade, seja como for, <br />
+      é a capacidade de sentir <br />
+      gratidão pelo agora.
+    </h1>
+  );
 }
 
 export default Home;
