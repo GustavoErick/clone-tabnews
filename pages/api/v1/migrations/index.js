a2dcb85e2ebd9c6526cd3f7105573a7a7b1b3860
GustavoErick <105515712+GustavoErick@users.noreply.github.com>
2026-02-23 20:31:16 +0000

    fix: `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,17 +3,27 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedMethods = ["GET", "POST"];
+
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).json({
+      error: `Method "${req.method}" not allowed`,
+    });
+  }
+
+  let dbClient;
 
   try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
     if (req.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       return res.status(200).json(pendingMigrations);
@@ -33,9 +43,8 @@ export default async function migrations(req, res) {
     }
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
   }
-
-  return res.status(405).end();
 }
